02316d6af46b46c5021ab80432690eab127b14a5
OleksijHr <hrymovskij@gmail.com>
2024-04-03 15:42:00 +0300

    create css

---
+++ src/js/render-functions.js
@@ -0,0 +1,6 @@
+// тут функції для відображення елементів інтерфейсу
+
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
